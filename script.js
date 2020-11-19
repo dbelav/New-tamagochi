1899120cb3b310a7042e67529bf4cdae9a4fa6c2
Danil <danilcka2234@gmail.com>
2020-11-19 16:20:41 +0200

    'red'

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let live;
 let cheking;
 let rand;
 let font = 22;
-
+let imageChildren = Array.from(document.querySelector('.image').children)
 
 let robert = {
     food: 0,
@@ -17,51 +17,33 @@ let robert = {
         return this[Object.keys(this)[n]];
     }
 };
-let all = {
-    food: document.getElementById('moodFood'),
-    clean: document.getElementById('moodClean'),
-    run: document.getElementById('moodrun'),
-    doctor: document.getElementById('moodDoctor'),
-    bar: document.getElementById('moodBar'),
-    work: document.getElementById('moodWork'),
-    key: function(n) {
-        return this[Object.keys(this)[n]];
-    },
-};
 
 function mood() {
-    let image = document.querySelector('.image')
-    Array.from(image.children).forEach(e =>{
-        e.style.display = 'none'
+    imageChildren.forEach(e => {
+        e.style.display = 'none';
     })
     for (let s in robert) {
         if (robert[s] === robert.food && robert.food <= 30) {
-            console.log('asdasd')
-            all.food.style.display = 'block'
+            imageChildren[0].style.display = 'block';
         }
-        else if (robert[s] === robert.clean && robert.clean <= 30) {
-            all.clean.style.display = 'block'
+        if (robert[s] === robert.clean && robert.clean <= 30) {
+            imageChildren[1].style.display = 'block';
         }
-        else if (robert[s] === robert.happy && robert.happy <= 30) {
-            all.run.style.display = 'block'
+        if (robert[s] === robert.happy && robert.happy <= 30) {
+            imageChildren[2].style.display = 'block';
         }
-        else if (robert[s] === robert.health && robert.health <= 30) {
-            all.doctor.style.display = 'block'
+        if (robert[s] === robert.health && robert.health <= 30) {
+            imageChildren[3].style.display = 'block';
         }
-        else if (robert[s] === robert.socialization && robert.socialization <= 30) {
-            all.bar.style.display = 'block'
+        if (robert[s] === robert.socialization && robert.socialization <= 30) {
+            console.log('asdds')
+            imageChildren[4].style.display = 'block';
         }
-        else if (robert[s] === robert.money && robert.money <= 30) {
-            all.work.style.display = 'block'
-        } 
+        if (robert[s] === robert.money && robert.money <= 30) {
+            imageChildren[5].style.display = 'block';
+        }
     }
-
-
-
-
 };
-
-
 
 function mode(stats) {
     for (let key in robert) {
@@ -70,8 +52,6 @@ function mode(stats) {
 }
 
 function check(arg) {
-    let loseMenu = document.querySelector('.lose')
-
     for (let key in robert) {
         if (robert[key] <= 0) {
             clearTimeout(interval);
@@ -84,21 +64,39 @@ function check(arg) {
                 socialization: 0,
                 money: 0,
             };
-            clearTimeout(live);
-            clearTimeout(cheking)
-            loseMenu.style.display = 'flex'
-            loseMenu.style.overflow = 'hidden'
-            setInterval(() => {
-                font += 0.5
-                loseMenu.style.fontSize = `${font}px`
-            }, 12)
+            lose()
         }
-        if (robert[key] > 100 && typeof arg === 'undefined') {
+        if (robert[key] > 100) {
             robert[key] = 100;
+        }else if(arg){
+
         }
     }
-    mood()
-    output()
+    mood();
+    output();
+}
+
+function asdd(arg){
+    if(arg){
+        console.log(arg)
+    }
+}
+asdd(123)
+
+function lose() {
+    let loseMenu = document.querySelector('.lose')
+    clearTimeout(live);
+    clearTimeout(cheking);
+    imageChildren.forEach(e => {
+        e.style.visibility = 'hidden';
+    })
+    loseMenu.style.display = 'flex';
+    restart.style.display = 'block'
+
+    setInterval(() => {
+        font += 0.5
+        loseMenu.style.fontSize = `${font}px`
+    }, 25);
 }
 
 function timeLived() {
@@ -122,38 +120,32 @@ function output() {
 }
 
 function displayStartMenu() {
-    let startMenu = document.querySelector('.mode')
-    startMenu.style.display = 'none'
+    let startMenu = document.querySelector('.mode');
+    startMenu.style.display = 'none';
 }
-
 
 let clickFluffy = document.getElementById('easy');
 clickFluffy.addEventListener('click', function() {
     displayStartMenu()
-    // random(50, 100);
-    random(40, 50);
-    check()
+    random(50, 100);
+    check();
     interval = setInterval(function() {
-        mode(3);
-        check()
+        mode(1);
+        check();
     }, 5000);
-
     live = setInterval(timeLived, 1000);
-
 });
 
 let clickLazy = document.getElementById('hard');
 clickLazy.addEventListener('click', function() {
     displayStartMenu()
     random(50, 70);
-    check()
+    check();
     interval = setInterval(function() {
         mode(5);
-        check()
+        check();
     }, 5000);
-
     live = setInterval(timeLived, 1000);
-
 });
 
 
@@ -161,13 +153,12 @@ let clickNinja = document.getElementById('ninja');
 clickNinja.addEventListener('click', function() {
     displayStartMenu()
     random(50, 150);
-    check()
+    startBusiness.style.display = 'block';
+    check();
     interval = setInterval(function() {
         mode(10);
-        check()
-        output()
+        check(5);
     }, 5000);
-    // setInterval(output, 1);
     live = setInterval(timeLived, 1000);
 });
 
@@ -176,28 +167,28 @@ let clickEat = document.getElementById('eat');
 clickEat.addEventListener('click', function() {
     robert.food += 40;
     robert.clean -= 30;
-    check()
+    check();
 });
 
 let clickWash = document.getElementById('wash');
 clickWash.addEventListener('click', function() {
     robert.clean += 40;
     robert.happy -= 20;
-    check()
+    check();
 });
 
 let clickHappy = document.getElementById('run');
 clickHappy.addEventListener('click', function() {
     robert.happy += 15;
     robert.food -= 10;
-    check()
+    check();
 });
 
 let visitDoctor = document.getElementById('doctor');
 visitDoctor.addEventListener('click', function() {
     robert.health += 30;
     robert.money -= 20;
-    check()
+    check();
 });
 
 let goBar = document.getElementById('bar');
@@ -206,7 +197,7 @@ goBar.addEventListener('click', function() {
     robert.money -= 20;
     robert.food += 10;
     robert.health -= 10;
-    check()
+    check();
 });
 
 let goWork = document.getElementById('work');
@@ -215,23 +206,31 @@ goWork.addEventListener('click', function() {
     robert.money += 50;
     robert.food -= 10;
     robert.health -= 10;
-    check()
+    check();
 });
 
-// let buyFood = document.getElementById('buy_food');
-// buyFood.addEventListener('click', function() {
-//     robert.money -= 20;
-//     robert.food += 20;
-// });
-
 let startBusiness = document.getElementById('buisness');
+startBusiness.style.display = 'none'
 startBusiness.addEventListener('click', function() {
     robert.socialization += 20;
     robert.money += 100;
     robert.health -= 100;
     robert.happy += 100;
-    check()
+    check();
+});
 
+let kill = document.getElementById('kill');
+kill.addEventListener('click', function() {
+    let gretta = document.querySelector('.gretta');
+    let cont = document.querySelector('.container');
+    let grettaText = document.querySelector('.gretta>span');
+    cont.style.visibility = 'hidden';
+    gretta.style.visibility = 'visible';
+    gretta.style.left = '50%';
+    gretta.addEventListener('transitionend', () => {
+        gretta.style.backgroundColor = 'rgba(255, 0, 0, 0.76)';
+        grettaText.style.display = 'inline';
+    })
 });
 
 let restart = document.getElementById('restart');
